47c9ab67f263435b036ce5ecabe090cfc103d1c0
Peyton Moss <pmoss94@gmail.com>
2022-11-29 10:44:21 -0700

    home screen added css base added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="" element={<HomeScreen />} />
+      </Routes>
     </div>
   );
 }
